d22248575ee1f2c674c743867bcd92dfe7be51f5
alvaroto23 <alvaro231099@gmail.com>
2024-12-27 14:50:46 +0100

    Prefabs slime y snot
    - Creación del prefab SNOT, posicionamiento y adición de componentes audio, script y collider
    - Añadidos al SLIME el tag y componentes animator, script, collision, input y audio
    - Configuración de las transiciones entre animaciones (en el script no hay nada aún)

--- Assets/Scripts/Snot.cs
+++ Assets/Scripts/Snot.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Character : MonoBehaviour
+public class Snot : MonoBehaviour
 {
 
 
 
     private void Awake()
     {
-        
+
     }
 
 
     void Start()
     {
-        
+
     }
 
 
     void Update()
     {
-        
+
     }
 }
