a356d167a725c8f8ca99f768c1451095c03ccc2d
alvaroto23 <alvaro231099@gmail.com>
2024-12-27 19:53:17 +0100

    Montar scripts del slime y del snot
    - El slime se mueve con inputSystem
    - El slime ejecuta la animación walk y jump
    - El snot se añade en una lista del slime cuando colisionan y se destruye

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed;
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D feet;
 
+
+    public List<int> snots = new List<int>();
+    private float moveInput;
 
 
     private void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-
-    void Start()
-    {
-        
-    }
-
 
     void Update()
     {
+        rb.velocity = new Vector2(moveInput * movementSpeed * Time.deltaTime, rb.velocity.y);
+        animator.SetBool("Move", moveInput != 0);
+        if (moveInput != 0 )
+        {
+            spriteRenderer.flipX = moveInput < 0;
+        }
         
     }
+    
+
+    private void OnMove (InputValue value)
+    {
+        moveInput = value.Get<float>();
+    }
+
+    private void OnJump(InputValue input)
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    //private void OnTriggerEnter2D(Collider collider)
+    //{
+        // No se como hacer que el trigger del hijo que son los pies (feet) detecten el suelo para ejecutar la transicion de jump a idle o walk
+    //}
 }
--- Assets/Scripts/Snot.cs
+++ Assets/Scripts/Snot.cs
@@ -7,21 +7,18 @@ public class Snot : MonoBehaviour
 {
 
 
-
-    private void Awake()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (collision.gameObject.CompareTag("Slime"))
+        {
+            Character slime = collision.GetComponent<Character>();
+            slime.snots.Add(1);
+            Destroy(gameObject);
+        }
     }
-
-
-    void Start()
-    {
-
-    }
-
 
     void Update()
     {
-
+        
     }
 }
