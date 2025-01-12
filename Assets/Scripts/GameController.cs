d7794b06147d78b68a6c967377f7b72095563755
alvaroto23 <alvaro231099@gmail.com>
2025-01-12 13:21:45 +0100

    Enable state of buttons on pause configuration

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Audio;
+
+public class GameController : MonoBehaviour
+{
+
+    //[SerializeField] private AudioMixer mixer;
+    private PlayerInput playerInput;
+    private GameObject[] menuButtons = new GameObject[3];
+    private bool pauseGame = false;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            menuButtons[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void ButtonsEnableState()
+    {
+        for (int i = 0; i < menuButtons.Length; i++)
+        {
+            if (menuButtons[i].CompareTag("SnotCounter") == false)
+            {
+                menuButtons[i].SetActive(pauseGame);
+            }
+            
+        }
+    }
+
+    public void returnGame()
+    {
+        pauseGame = !pauseGame;
+        //mixer.SetFloat("LowpassFreq", 22000);
+        Time.timeScale = 1f;
+        ButtonsEnableState();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnPause()
+    {
+        pauseGame = !pauseGame;
+
+        if (pauseGame)
+        {
+            //mixer.SetFloat("LowpassFreq", 500);
+            Time.timeScale = 0f;
+            ButtonsEnableState();
+
+        }
+
+        else if (!pauseGame)
+        {
+            //mixer.SetFloat("LowpassFreq", 22000);
+            Time.timeScale = 1f;
+            ButtonsEnableState();
+        }
+    }
+}
