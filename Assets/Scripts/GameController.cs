cffe99b2a1dd2d701eed463a7ed16127b0b17ff4
SerrgioM <sergiolmoraalumno@gmail.com>
2025-01-14 21:12:48 +0100

    Error 3.0

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Audio;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
