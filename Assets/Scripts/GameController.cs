5f26869426d2eb8f1b23c515902617f6cf90271c
alvaroto23 <alvaro231099@gmail.com>
2025-01-14 21:21:21 +0100

    Revert "Error 3.0"
    This reverts commit 432a39995a85fb1a772eea34bf8e5ac2a91ca8d4.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Audio;
+using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
